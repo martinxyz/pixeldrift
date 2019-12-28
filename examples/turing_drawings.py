3654d7d70323ba27fe91209c8a54df3744f84653
Martin Renold <martin@log2.ch>
2019-12-28 22:28:58 +0100

    python coding style and whitespace
    Thus spake the Lord: Thou shalt indent with four spaces. No more, no less.
    Four shall be the number of spaces thou shalt indent, and the number of thy
    indenting shall be four. Eight shalt thou not indent, nor either indent thou
    two, excepting that thou then proceed to four. Tabs are right out.

--- examples/turing_drawings.py
+++ examples/turing_drawings.py
@@ -1,35 +1,33 @@
 #!/usr/bin/env python3
-import numpy as np
 import time
 from numpy.random import randint
 import pixeldrift
 from pixeldrift import World, TuringHeads
 
+
 def random_lut():
-  input_bits = 7
-  state_bits = 4
-  size = 2**(input_bits+state_bits)
-  next_output = randint(0, 2, size)
-  # next_output = np.ones(size, dtype='uint8')
-  # next_movement = np.ones(size, dtype='uint8') * d
-  next_movement = randint(0, 6, size)
-  next_state = randint(0, 2**4, size)
-  # next_state = np.ones(size, dtype='uint8') * 0xFF
+    input_bits = 7
+    state_bits = 4
+    size = 2**(input_bits+state_bits)
+    next_output = randint(0, 2, size)
+    # next_output = np.ones(size, dtype='uint8')
+    # next_movement = np.ones(size, dtype='uint8') * d
+    next_movement = randint(0, 6, size)
+    next_state = randint(0, 2**4, size)
+    # next_state = np.ones(size, dtype='uint8') * 0xFF
 
-
-  # bool get_movement() { return (value >> 1) & ((1<<3)-1); }
-  return (next_output | (next_movement << 1) | (next_state << 4)).astype('uint8')
-
-
+    # bool get_movement() { return (value >> 1) & ((1<<3)-1); }
+    res = next_output | (next_movement << 1) | (next_state << 4)
+    return res.astype('uint8')
 
 w = World()
 th = TuringHeads()
 th.add_head(w, pixeldrift.tile_size//2, pixeldrift.tile_size//2)
 t0 = time.time()
 for j in range(50):
-  th.set_lut(random_lut())
-  for i in range(10):
-    w.tick(10000)
+    th.set_lut(random_lut())
+    for i in range(10):
+        w.tick(10000)
 print('time.time() - t0:', time.time() - t0)
 # pixeldrift.render(w.get_particles(), f'output_{j:04}.png')
 pixeldrift.render(w.get_particles(), f'output.png')
